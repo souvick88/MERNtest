11e76375ecf6abbe6b61f0408eb223ba3c63aa64
souvick C <souvickdesign1@gmail.com>
2020-07-06 16:31:34 -0400

    second commit

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+//Connect database
+
+connectDB();
+
 app.get('/', (req, res) => res.send('API running'));
+
+//Define Routes
+
+// app.use('/api/users', require('./routes/api/users'));
+// // app.use('/api/auth', require('./routes/api/auth'));
+// // app.use('/api/posts', require('./routes/api/posts'));
+// // app.use('/api/profiles', require('./routes/api/profiles'));
+
+// app.use('/api/users', require('./routes/api/users'));
+// app.use('/api/auth', require('./routes/api/auth'));
+// app.use('/api/profile', require('./routes/api/profile'));
+// app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.port || 5000;
 
